3fb3cb4f804d2cbd166211de4bba3a0a3d68664b
aldoslonder2107-stack <aldoslonder2107@gmail.com>
2025-08-04 16:17:41 +0700

    add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatAssistant from './ChatAssistant';
+
+function App() {
+  return (
+    <div>
+      <h1>Asisten Aldo Aktif</h1>
+      <ChatAssistant />
+    </div>
+  );
+}
+
+export default App;
